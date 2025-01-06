58c920d9c5b7c5f09638664137a2cc399e9ef867
Jesus Herrera <jesusherrera1594@gmail.com>
2025-01-06 16:58:05 -0600

    add conditionals

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 fn main() {
-    println!("Introduce your name: ");
-    let mut name: String = String::new(); // to set a empty string
-
-
-    // to capture the input from the user
-    std::io::stdin().read_line(&mut name).unwrap();
-    // obtatain the age from the user and convert it to number
     println!("Introduce your age: ");
-    let mut age: String = String::new();
+    let mut age: String = String::new(); // to set a empty string
     std::io::stdin().read_line(&mut age).unwrap();
-    let age_number: u8 = age.trim().parse().unwrap();
-    println!("Welcome! {}", name.trim().to_string());
-    print!("Your age is: {}", age_number);
 
-    // the {} are positional arguments
-    // println!("Hello my name is {} and my age is: {}", name, age);
+    let age_number: u8 = age.trim().parse().unwrap();
+
+    if age_number >= 18 {
+        println!("you can access to the club");
+    } else {
+        println!("you can't access to the club");
+    }
+
+    println!("You are {} yrs old", age_number)
+
+    
 }
