7e842a5de0228d010c3589d38cb7a95ed8f4eec1
Jesus Herrera <jesusherrera1594@gmail.com>
2025-01-08 16:24:22 -0600

    create guessing minigame

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 fn main() {
-    println!("Introduce your age: ");
-    let mut age: String = String::new(); // to set a empty string
-    std::io::stdin().read_line(&mut age).unwrap();
+    let number_1 = 123;
+    let number_2 = 321;
+    let sum = number_1 + number_2;
 
-    let age_number: u8 = age.trim().parse().unwrap();
+    loop {
+        // print the two numbers on the terminal
+        println!("write the answer of : {} + {}", number_1, number_2);
 
-    if age_number >= 18 {
-        println!("you can access to the club");
-    } else {
-        println!("you can't access to the club");
+        // get the answer from the user
+        let mut answer: String = String::new();
+        std::io::stdin().read_line( &mut answer).unwrap();
+
+        let user_answer: i32 =  answer.trim().parse().unwrap();
+
+        if user_answer == sum {
+            println!("correct answer");
+            break;
+        } else {
+            println!("wrong answer, try again");
+        }
     }
-
-    println!("You are {} yrs old", age_number)
-
     
 }
